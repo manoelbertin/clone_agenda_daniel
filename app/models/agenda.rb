dbec39901cd939852e810766932bd909cf18cfa1
manoel bertin <manoeltirj@gmail.com>
2023-08-17 20:09:14 -0300

    instalada a gem simple_form

--- app/models/agenda.rb
+++ app/models/agenda.rb
@@ -1,3 +1,6 @@
 class Agenda < ApplicationRecord
   belongs_to :professional
+  has_many :agenda_slots 
+
+  accepts_nested_attributes_for :agenda_slots, reject_if: ->(attributes){ attributes['name'].blank? }, allow_destroy: true
 end

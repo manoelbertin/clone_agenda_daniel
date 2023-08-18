3df584faa2d1d47068f2ee994cd7e10dd4bbebd6
manoel bertin <manoeltirj@gmail.com>
2023-08-18 06:49:25 -0300

    aqui no model agenda eu coloco para rejeitar os fields vazios para não salvar.

--- app/models/agenda.rb
+++ app/models/agenda.rb
@@ -2,5 +2,5 @@ class Agenda < ApplicationRecord
   belongs_to :professional
   has_many :agenda_slots 
 
-  accepts_nested_attributes_for :agenda_slots, reject_if: ->(attributes){ attributes['name'].blank? }, allow_destroy: true
+  accepts_nested_attributes_for :agenda_slots, reject_if: :all_blank, allow_destroy: true
 end
